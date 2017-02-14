677693ee2c0d6634c40a0fd8dbb0260858ad8bbf
Marcel Baumann <Littellittel@gmx.de>
2017-02-14 16:13:36 +0100

    basic class to save all significant data for a match

---
+++ src/Match.java
@@ -0,0 +1,29 @@
+
+/**
+ * the "Match" class is used to save data for a match between two participants
+ * 
+ * @author Marcel Baumann
+ *
+ */
+public class Match {
+	
+	/**
+	 * the first participant
+	 */
+	public Participant participant1;
+	
+	/**
+	 * the second participant
+	 */
+	public Participant participant2;
+	
+	/**
+	 * the points for the first participant
+	 */
+	public int participant1Points;
+	
+	/**
+	 * the points for the second participant
+	 */
+	public int participant2Points;
+}
